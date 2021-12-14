108f52064f9d9005833482d9de86adc8268bfb57
Anupam <anupam.saini18@gmail.com>
2021-12-14 21:47:15 +0530

    Fifth commit

--- double_linked_list.c
+++ double_linked_list.c
@@ -6,4 +6,8 @@ struct node
     int info;
     struct node *next;
 };
-
+struct node *create_list(struct node *start);
+void display(struct node *start);
+struct node *addtoempty(struct node *start,int data);
+struct node *addatbeg(struct node *start,int data);
+struct node *addatend(struct node *start,int data);
