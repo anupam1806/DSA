68f974195ec66e8473259cf9e594a8b96cca3969
Anupam <anupam.saini18@gmail.com>
2021-12-15 22:10:16 +0530

    Sixth commit

--- double_linked_list.c
+++ double_linked_list.c
@@ -151,3 +151,24 @@ struct node *addatend(struct node *start,int data){
     tmp->next=NULL;
     return start;
 }
+struct node *addafter(struct node *start,int data,int item){
+    struct node *tmp,*p;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info=data;
+    p=start;
+    while(p!=NULL){
+        if (p->info == item)
+        {
+            tmp->prev=p;
+            tmp->next=p->next;
+            if(p->next!=NULL){
+                p->next->prev = tmp;
+            }
+            p->next=tmp;
+            return start;
+        }
+        p=p->next;
+    }
+    printf("%d not present in the list\n",item);
+    return start;
+}
