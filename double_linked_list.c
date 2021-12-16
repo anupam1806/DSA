d0695463fa4cceabf4ce578af9d37bcabffa8b96
Anupam <anupam.saini18@gmail.com>
2021-12-16 22:29:38 +0530

    Seven commit

--- double_linked_list.c
+++ double_linked_list.c
@@ -248,3 +248,19 @@ struct node *del(struct node *start,int data)
     return start;
     }
 }
+struct node *reverse(struct node *start){
+    struct node *p1,*p2;
+    p1=start;
+    p2=p1->next;
+    p1->next=NULL;
+    p1->prev=p2;
+    while(p2!=NULL){
+        p2->prev = p2->next;
+        p2->next=p1;
+        p1=p2;
+        p2=p2->prev;
+    }
+    start=p1;
+    printf("List reversed\n");
+    return start;
+}
