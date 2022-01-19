b228828b62e40acfb03310d84289241c8561db4f
Anupam <anupam.saini18@gmail.com>
2022-01-19 23:30:17 +0530

    First commit

---
+++ Love Babbar/Q3.c
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+
+ // } Driver Code Ends
+//User function Template for C
+void swap(int *a, int *b)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    // finding the pivot element and partition the array along that
+    int randomPartition(int arr[], int l, int r)
+    {
+        int n = r-l+1;
+        int pivot = rand() % n;
+        swap(&arr[l + pivot], &arr[r]);
+        return partition(arr, l, r);
+    }
+int kthSmallest(int arr[], int l, int r, int k)
+    {
+        if (k > 0 && k <= r - l + 1)
+        {
+            // find a position for random partition
+            int pos = randomPartition(arr, l, r);
+            
+            // if this pos gives the kth smallest element, then return
+            if (pos-l == k-1)
+                return arr[pos];
+                
+            // else recurse for the left and right half accordingly
+            if (pos-l > k-1)  
+                return kthSmallest(arr, l, pos-1, k);
+            return kthSmallest(arr, pos+1, r, k-pos+l-1);
+        }
+    
+        return 99999999;
+    }
+int partition(int arr[], int l, int r)
+    {
+        int x = arr[r], i = l;
+        for (int j = l; j <= r - 1; j++)
+        {
+            if (arr[j] <= x)
+            {
+                swap(&arr[i], &arr[j]);
+                i++;
+            }
+        }
+        swap(&arr[i], &arr[r]);
+        return i;
+    }
+// { Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    scanf("%d",&test_case);
+    while(test_case--)
+    {
+        int number_of_elements;
+        scanf("%d", &number_of_elements);
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            scanf("%d", &a[i]);
+            
+        int k;
+        scanf("%d",&k);
+        printf("%d",kthSmallest(a, 0, number_of_elements-1, k));
+        printf("\n");
+    }
+    return 0;
+}  // } Driver Code Ends
